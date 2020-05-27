e63af4fab3dd4e87fcdbcc6fa968e69cb82933ba
itdboy <itdboy@gmail.com>
2020-05-27 19:01:30 +0700

    3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import home from './components/home' ;
+import home from './components/home';
 import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 import webboard from './components/webboard';
 import contactus from './components/contactus';
@@ -8,31 +8,40 @@ import urlnotfound from './components/urlnotfound';
 
 function App() {
   return (
-     <BrowserRouter>
-<div id="navMain"  >
-<ul >
-  <li >
-    <Link to="/">หน้าแรก</Link>
-  </li>
-  <li>
-    <Link to="/webboard">เว็บบอร์ด</Link>
-  </li>
-  <li>
-    <Link to="/contactus">ติดต่อเรา</Link>
-  </li>
-</ul>
-<div>
-<Switch>
-  <Route exact path="/" component={home}/>
-  <Route exact path="/webboard" component={webboard}/>
-  <Route exact path="/contactus" component={contactus}/>
-  <Route component={urlnotfound}/>
 
-</Switch>
-</div>
-</div>
+    <BrowserRouter>
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="http://www.itd-cp0304.com">ITD</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse"
+          data-target="#navMain" aria-controls="navMain"
+          aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-     </BrowserRouter>
+        <div id="navMain" class="collapse navbar-collapse"  >
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active" >
+              <Link class="nav-link" to="/">หน้าแรก</Link>
+            </li>
+            <li>
+              <Link class="nav-link" to="/webboard/1001">เว็บบอร์ด</Link>
+            </li>
+            <li>
+              <Link class="nav-link" to="/contactus">ติดต่อเรา</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div>
+        <Switch>
+          <Route exact path="/" component={home} />
+          <Route exact path="/webboard/:kid" component={webboard} />
+          <Route exact path="/contactus" component={contactus} />
+          <Route component={urlnotfound} />
+
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/webboard.js
+++ src/components/webboard.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-function webboard(){
+function webboard({ match }) {
     return (
         <div>
             <h1>หน้า webboard</h1>
+            <p>
+                กระทู้ปัจจุบัน : {match.params.kid}
+            </p>
         </div>
     )
 }
